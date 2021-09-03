d0c0691b3d032213e47df6527c1f417b7bf22bc7
Felipe Silva <felipe.dacs@gmail.com>
2021-09-02 23:44:04 -0300

    :tv: add aula 2

---
+++ teste-avaliador.php
@@ -0,0 +1,14 @@
+<?php
+
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+
+require 'vendor/autoload.php';
+
+$leilao = new Leilao('Fiat 147 0km');
+
+$maria = new Usuario('Maria');
+$joao = new Usuario('Joao');
+
+$leilao->recebeLance(new \Alura\Leilao\Model\Lance($joao, 2000));
+$leilao->recebeLance(new \Alura\Leilao\Model\Lance($maria, 2500));
